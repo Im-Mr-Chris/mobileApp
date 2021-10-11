9943cde0341b848452f377cc6ca8fa4ad2ca9a17
Ahmad Mazaal <ahmad.hmazaal@gmail.com>
2021-10-11 15:17:49 +0300

    feature: add new splash screen & changed BitClout titles to DeSo in Intro screen (#223)

--- src/screens/introduction/components/introContent.tsx
+++ src/screens/introduction/components/introContent.tsx
@@ -12,12 +12,12 @@ export const introduction: IntroductionElement[] = [
     {
         title: 'Welcome!',
         imageUri: require('../../../../assets/intro1.png'),
-        description: <Text>BitClout is a new type of social network that mixes speculation and social media, and it’s built from the ground up as its own custom blockchain. Its architecture is similar to Bitcoin.{'\n'}{'\n'}Like Bitcoin, BitClout is a fully open-source project and there is no company behind it; it’s just coins and code.
+        description: <Text>DeSo is a new type of social network that mixes speculation and social media, and it’s built from the ground up as its own custom blockchain. Its architecture is similar to Bitcoin.{'\n'}{'\n'}Like Bitcoin, DeSo is a fully open-source project and there is no company behind it; it’s just coins and code.
             For more info, check
             {' '}<Text
                 onPress={() => Linking.openURL('https://docs.bitclout.com/')}
                 style={[{ fontWeight: '500' }, themeStyles.linkColor]}>
-                Bitclout docs
+                DeSo docs
             </Text>
         </Text>,
     },
@@ -29,24 +29,24 @@ export const introduction: IntroductionElement[] = [
     {
         title: 'What is CloutFeed?',
         imageUri: require('../../../../assets/intro2.png'),
-        description: 'CloutFeed is a user-friendly mobile app that allows users to access BitClout from the convenience of their smartphones. Say goodbye to computer screens! Make the most out of BitClout with the all-new mobile experience'
+        description: 'CloutFeed is a user-friendly mobile app that allows users to access DeSo from the convenience of their smartphones. Say goodbye to computer screens! Make the most out of DeSo with the all-new mobile experience'
     },
     {
         title: 'Why CloutFeed?',
         imageUri: require('../../../../assets/intro3.png'),
         description: <Text>
-            CloutFeed is the first BitClout mobile application. It is
+            CloutFeed is the first DeSo mobile application. It is
             {' '}<Text
                 onPress={() => Linking.openURL('https://github.com/CloutFeed/mobileApp')}
                 style={[{ fontWeight: '500' }, themeStyles.linkColor]}>
                 open-source
-            </Text> and has been vetted multiple times.{'\n'}{'\n'}CloutFeed maximizes your BitClout experience with countless exclusive features that are just available for CloutFeed's users like CloutTags, post notifications and much more.
+            </Text> and has been vetted multiple times.{'\n'}{'\n'}CloutFeed maximizes your DeSo experience with countless exclusive features that are just available for CloutFeed's users like CloutTags, post notifications and much more.
         </Text>
     },
     {
         title: 'Let\'s go!',
         imageUri: require('../../../../assets/intro5.png'),
-        description: <Text>What are you waiting for? Start earning BitClout and socializing with your friends now! Stay updated with the latest news with native notifications.{'\n '} Don't have an account?
+        description: <Text>What are you waiting for? Start earning DeSo and socializing with your friends now! Stay updated with the latest news with native notifications.{'\n '} Don't have an account?
             {' '}<Text
                 onPress={() => Linking.openURL('https://bitclout.com/')}
                 style={[{ fontWeight: '500' }, themeStyles.linkColor]}>
